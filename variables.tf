e34ee188e0c891e7986d698f5050d4e6e5ff8926
deepak-kr7 <deepaknsn7@gmail.com>
2026-08-12 00:15:35 +0530

    key

--- variables.tf
+++ variables.tf
@@ -39,3 +39,13 @@ variable "storage_accounts" {
     tags                     = optional(map(string), {})
   }))
 }
+
+variable "ai" {
+  default = "B7iEUe5TCSRayYqbwR9itVIgQHNJynFdHfrJPIUyIFMc5ti19MZ8JQQJ99CHACYeBjFXJ3w3AAABACOGQk3i"
+  
+}
+
+variable "aii" {
+  default = "Dq0VCJnZwMmMq7Gl0b4CO779uq1j7wpqM8QZfXW1491pljiXShRrJQQJ99CHACYeBjFXJ3w3AAABACOGoAbG"
+  
+}
