43eb2193b59a17794472eaec92aece5cceb7bb84
deepak-kr7 <deepaknsn7@gmail.com>
2026-08-12 00:56:58 +0530

    new key3

--- variables.tf
+++ variables.tf
@@ -44,7 +44,7 @@ variable "storage_accounts" {
 #API key from Azure Open AI
 variable "ai" {
   default = "B7iEUe5TCSRayYqbwR9itVIgQHNJynFdHfrJPIUyIFMc5ti19MZ8JQQJ99CHACYeBjFXJ3w3AAABACOGQk3i"
-type = string
+  type    = string
 }
 
 variable "aii" {
